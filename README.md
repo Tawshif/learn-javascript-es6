fee65e8efafc17383ed4f5a6ba62279c86af4d63
tawshif <anikphp@gmail.com>
2017-06-20 01:36:22 +0600

    video 5

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
 # Practice based on wes bos javascript for every one tutorial
-
-### variables
-
-```javascript
-var width = 100;
-
-let height = 200;
-
-const key = 'abc123';
-```
-- var is a function scope but
-- let and const are block scope which works inside this { } brackets
-- 
--- app/index.js
+++ app/index.js
@@ -1,58 +1,23 @@
-var width = 100;
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-// function scoping
+/*
+* Store picker
+*/
 
-// function setWidth() {
-//     var width = 300;
-//     console.log(width);
-// }
+var StorePicker = React.createClass({
 
-// setWidth();
+    render: function(){
+        return (
 
-// console.log(width);
+            <form>
+        		<h2>Another label</h2>
+        		<input type="text" className="form-control" />
+            	<input type="submit" />
+            </form>
+        )
+    }
 
+})
 
-// ---------------------------------
-
-
-// var age = 100;
-
-// if (age > 12) {
-//     const dogYears = age * 7;
-//     console.log(`you are ${dogYears} dog years old`);
-// }
-
-// console.log(dogYears);
-
-
-
-// const key = 'abc123';
-// let points = 50;
-// let wineer = false;
-
-// if (points > 40) {
-
-// } else {
-
-// }
-
-for (let age = 1; age < 10; age++) {
-
-    setTimeout(function() {
-        console.log(`i am ${age} years old`);
-    }, 200);
-
-}
-
-let c = 'yes';
-const markup = `<h1>He is now ${5*6}</h1>
-   ${ feat(c) }
-`;
-
-function feat(d) {
-    return (
-            `<li>${d.map(key => `<span> ${key}</span>`).join('')}</li>`
-    );
-}
-
-document.body.innerHTML = markup;
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
